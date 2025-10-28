78801eff5e419aa574de739751ece625cf0d761d
Paraspandey-debugs <p.pandey250806@gmail.com>
2025-10-28 12:25:34 +0530

    Refactor Client and Server classes to use JFrame instance for UI management and improve code structure

--- src/chatting/application/Client.java
+++ src/chatting/application/Client.java
@@ -1,26 +1,30 @@
 
 package chatting.application;
-import java.awt.Color;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.*;
+import java.net.Socket;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.net.*;
+import java.io.*;
 
-public class Client extends JFrame implements ActionListener {
+public class Client implements ActionListener {
     JTextField text;
-    JPanel a1;
-    Box vertical = Box.createVerticalBox();
+    static JPanel a1;
+    static Box vertical = Box.createVerticalBox();
+    static DataOutputStream dout;
+    static JFrame f = new JFrame();
 
     Client() {
-        setLayout(null);
+        f.setLayout(null);
 
         JPanel p1 = new JPanel();
         p1.setBackground(new Color(7,94,84));
         p1.setBounds(0,0,450,70);
         p1.setLayout(null);
-        add(p1);
+        f.add(p1);
 
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/3.png"));
         Image i2 = i1.getImage().getScaledInstance(25, 25,Image.SCALE_DEFAULT);
@@ -31,7 +35,7 @@ public class Client extends JFrame implements ActionListener {
 
         back.addMouseListener(new MouseAdapter(){
             public void mouseClicked(MouseEvent ae){
-                setVisible(false);
+                f.setVisible(false);
                 System.exit(0);
             }
         });
@@ -78,45 +82,51 @@ public class Client extends JFrame implements ActionListener {
 
         a1 = new JPanel();
         a1.setBounds(5,75,440 , 570);
-        setUndecorated(true);
-        add(a1);
+        f.setUndecorated(true);
+        f.add(a1);
 
         text = new JTextField();
         text.setBounds(5,655,310,40);
         text.setFont(new Font("SAN_SERIF" , Font.BOLD , 15 ));
-        add(text);
+        f.add(text);
 
         JButton send = new JButton("Send");
         send.setBounds(320,655,123,40);
         send.setBackground(new Color(7,94,84));
         send.addActionListener(this);
         send.setFont(new Font("SAN_SERIF" , Font.BOLD , 15 ));
-        add(send);
+        f.add(send);
 
-        setSize(450,700);
-        setLocation(800,50);
-        getContentPane().setBackground(Color.WHITE);
-        setVisible(true);
+        f.setSize(450,700);
+        f.setLocation(800,50);
+        f.getContentPane().setBackground(Color.WHITE);
+        f.setVisible(true);
     }
 
     public void actionPerformed(ActionEvent ae) {
-        String out = text.getText();
-        JPanel p2 = formatLabel(out);
+        try{
+            String out = text.getText();
+            JPanel p2 = formatLabel(out);
 
-        a1.setLayout(new BorderLayout());
+            a1.setLayout(new BorderLayout());
 
-        JPanel right = new JPanel(new BorderLayout());
-        right.add(p2, BorderLayout.LINE_END);
-        vertical.add(right);
-        vertical.add(Box.createVerticalStrut(15));
+            JPanel right = new JPanel(new BorderLayout());
+            right.add(p2, BorderLayout.LINE_END);
+            vertical.add(right);
+            vertical.add(Box.createVerticalStrut(15));
 
-        a1.add(vertical, BorderLayout.PAGE_START);
+            a1.add(vertical, BorderLayout.PAGE_START);
 
-        text.setText("");
+            dout.writeUTF(out);
 
-        repaint();
-        invalidate();
-        validate();
+            text.setText("");
+
+            f.repaint();
+            f.invalidate();
+            f.validate();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     public static JPanel formatLabel(String out) {
@@ -144,5 +154,30 @@ public class Client extends JFrame implements ActionListener {
 
     public static void main(String[] args){
         new Client();
+
+        try {
+            Socket s = new Socket("127.0.0.1", 6001);
+            DataInputStream din = new DataInputStream(s.getInputStream());
+            dout = new DataOutputStream(s.getOutputStream());
+
+            while (true) {
+                a1.setLayout(new BorderLayout());
+                String msg = din.readUTF();
+                JPanel panel = formatLabel(msg);
+
+                JPanel left = new JPanel(new BorderLayout());
+                left.add(panel, BorderLayout.LINE_START);
+                vertical.add(left);
+
+                vertical.add(Box.createVerticalStrut(15));
+                a1.add(vertical, BorderLayout.PAGE_START);
+
+
+                f.validate();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
